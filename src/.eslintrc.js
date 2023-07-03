172a34195476e875d38eae4111c70f38376ebd18
bbbii <hyeongjin1326@gmail.com>
2023-07-03 16:53:41 +0900

    init: [김형진] CD 구성

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,10 +1,9 @@
 module.exports = {
-  extends: "standard",
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:vue/essential", "standard"],
+  extends: ["plugin:vue/essential"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
